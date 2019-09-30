08299c6f3285e91c00cdd5a0bb2b88f08f10affc
小泉大夢 <abn21029@gmail.com>
2019-09-30 16:22:50 +0900

    create:root

---
+++ test/helpers/application_helper_test.rb
@@ -0,0 +1,9 @@
+require 'test_helper'
+
+class ApplicateionHelperTest<ActionView::TestCase
+    test "full title helper" do
+        assert_equal full_title, "Ruby on Rails Tutorial Sample App"
+    end
+        
+    
+end
